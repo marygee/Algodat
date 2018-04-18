3b657594f8f3579c6587917003a8eb336563fd77
marygee <38464170+marygee@users.noreply.github.com>
2018-04-18 17:47:25 +0200

    Add files via upload

--- spanning.py
+++ spanning.py
@@ -52,58 +52,57 @@ def ad_edges(string):
     heappush(cities[i_one].edges, ([distance, city_two]))
     heappush(cities[i_two].edges, ([distance, city_one]))
 
-def read_file():
+def read_file(file):
     #lines = sys.stdin.readlines()                 # till forsete
-    #f = open(file, 'r')                             # kör en egen fil
-    #lines = f.readlines()
-    for arg in sys.argv:
+    f = open(file, 'r')                             # kör en egen fil
+    lines = f.readlines()
+
+
+    #for arg in sys.argv:
+    for line in lines:
         #line_name = line.split()                   #Används för bokstavsfilen
         #if len(line_name) is 1:                    #Funkar för bokstavsgrejen
-        line = arg
+        #line = arg
 
         if line[-2] != "]":
             edges = []
             line = line[:-2]
+
             if line[0] == "\"":
                 line = line[1:-1]
             make_city(line, edges)
-        else:
 
+        else:
             line = line.split("[")
             ad_edges(line)
 
 
-
-#read_file('tinyEWG-alpha.txt')
-read_file()
-
-
 #Hitta minimum spanning tree med Prims algoritm
-def find_MST(input):
-    input_cities = input
-    #MST = []
+def find_MST():
 
+    input_cities = cities
     MST_cities = []
     MST_weight = 0
     MST_edges = ""
-    min_weight = sys.maxsize
-    first_node = None
+#    min_weight = sys.maxsize
+    first_node = input_cities[0]
 
-    for city in input_cities:
-        curr_edge = city.edges[0]
-        if curr_edge[0] < min_weight:
-            min_weight = curr_edge[0]
-            first_node = city
+#    for city in input_cities:
+#        curr_edge = city.edges[0]
+#        if curr_edge[0] < min_weight:
+#            min_weight = curr_edge[0]
+#            first_node = city
 
     input_cities.remove(first_node)
     MST_cities.append(first_node.name)
-    actual_edges = first_node.edges
-    heapify(actual_edges)
-    for entry in actual_edges:              #Lägger till föräldern till edges
+    all_edges = first_node.edges
+    heapify(all_edges)
+    for entry in all_edges:              #Lägger till föräldern till edges
         entry.append(first_node.name)
 
+    #print(all_edges)
     while input_cities != []:                   #Vill hålla på tills alla städer finns med
-        possible_next_edge = heappop(actual_edges)   #Får reda på vilket det kostaste avståndet är och till vilken stad. Den edgen tas bort från listan
+        possible_next_edge = heappop(all_edges)   #Får reda på vilket det kostaste avståndet är och till vilken stad. Den edgen tas bort från listan
 
         if not possible_next_edge[1] in MST_cities:         #Kollar om staden redan finns i MST
             i = find_index(possible_next_edge[1], input_cities)        #Hittar var i cities-listan nästa stad som ska läggas till finns
@@ -114,18 +113,23 @@ def find_MST(input):
 
             for entry in next_city.edges:
                 entry.append(next_city.name)
-                heappush(actual_edges, entry)
+                heappush(all_edges, entry)
 
             MST_edges = MST_edges + possible_next_edge[2] + "--" + possible_next_edge[1] + " [" + str(possible_next_edge[0]) + "] \n"
 
 
 
-    print(MST_edges)
+    #print(MST_edges)
     print(MST_weight)
+    return MST_weight
+
+# read_file('tinyEWG-alpha.txt')
+#read_file('USA-highway-miles.txt')
+text_file = sys.argv[1]
+read_file(text_file)
 
 
-
-find_MST(cities)
+find_MST()
 
 #print("--- %s seconds --- to finish" % (time.time() - start_time))
 
